91078aed61e3c072be42ede377787a7213daf1a1
Ali Guefrech <guefrech.ali@gmail.com>
2019-01-30 15:50:24 +0100

    theme

--- onlineshopping/src/main/java/net/gue/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/gue/onlineshopping/controller/PageController.java
@@ -17,14 +17,41 @@ public class PageController {
 	public ModelAndView index() {
 		
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","welcome to springmvc");
-		return mv ;
-		
-		
-		
-	
-		
+		mv.addObject("title","home");
+		mv.addObject("userClickHome",true);
+		return mv ;	
 	}			
+	@RequestMapping(value= {"/About"})
+	public ModelAndView About() {
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","About");
+		mv.addObject("userClickAbout",true);
+		return mv ;	
+	}	
+	
+	@RequestMapping(value= {"/Contact"})
+	public ModelAndView Contact() {
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contacts Us");
+		mv.addObject("userClickContact",true);
+		return mv ;	
+	}	
+	
+	@RequestMapping(value= {"/Product"})
+	public ModelAndView Product() {
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Product");
+		mv.addObject("userClickProduct",true);
+		return mv ;	
+	}	
+	
+	
+	
+	
+	
 	@RequestMapping(value= "/test")
 	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
 		if (greeting==null) {
